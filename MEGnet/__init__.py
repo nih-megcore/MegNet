b0ca81fdeb16f52f5f8a63c04fad86ee5f6e3287
Jeff Stout <stoutjd@nih.gov>
2023-03-20 21:47:57 -0400

    Add version to module import

--- MEGnet/__init__.py
+++ MEGnet/__init__.py
@@ -1,1 +1,10 @@
 from . import prep_inputs
+import os.path as op
+
+version_path=op.join(op.dirname(__file__), 'version')
+
+
+with open(version_path) as f:
+    __version__=f.readlines()[0]
+    __version__=__version__.replace('\n','')
+#__version__='0.2'
