6bcbae214148480ca3e1e28cc0cca3f73ff1f9a0
Jeff Stout <stoutjd@nih.gov>
2023-05-30 11:53:03 -0400

    Change model to save on each epoch

--- MEGnet/prep_inputs/new_train_model.py
+++ MEGnet/prep_inputs/new_train_model.py
@@ -21,7 +21,7 @@ from scipy.io import loadmat
 import numpy as np
 import copy
 import pickle
-from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
 from matplotlib import pyplot as plt 
 from sklearn.metrics import confusion_matrix
 import sklearn
@@ -29,17 +29,17 @@ from tensorflow_addons.metrics import F1Score
 import tensorflow as tf
 from tensorflow import one_hot
 from tensorflow import keras
-from MEGnet.megnet_utilities import fPredictChunkAndVoting
+from MEGnet.megnet_utilities import fPredictChunkAndVoting, fGetStartTimesOverlap
 
 if __name__=='__main__':
     import argparse
     parser=argparse.ArgumentParser()
     parser.add_argument('-output_dir')
-    parser.add_argument('-normalize',
-                        action='store_true',
-                        help='''Apply min/max normalization on time series
-                        This will be set to +/- 15 which is the tested mean'''
-                        )
+    # parser.add_argument('-normalize',
+    #                     action='store_true',
+    #                     help='''Apply min/max normalization on time series
+    #                     This will be set to +/- 15 which is the tested mean'''
+    #                     )
     parser.add_argument('-weights',
                         default=3,
                         help='Minority weighting for loss calc')
@@ -57,10 +57,10 @@ if __name__=='__main__':
                         )
     args = parser.parse_args()
     output_dir = args.output_dir
-    if args.normalize==True:
-        NORMALIZE=True
-    else:
-       NORMALIZE=False
+    # if args.normalize==True:
+    #     NORMALIZE=True
+    # else:
+    #    NORMALIZE=False
     if args.extended_model==True:
         MODEL_EXT=True
     else:
@@ -78,7 +78,7 @@ if __name__=='__main__':
     if not os.path.exists(output_dir): os.mkdir(output_dir)
 
 # To initialize these not from commandline
-# NORMALIZE=True ; MODEL_EXT=False ; BURN_IN = False ; FREEZE_MIDS  = False; class_weights={0:1, 1:10, 2:10, 3:10}
+# NORMALIZE=True ; MODEL_EXT=False ; BURN_IN = False ; FREEZE_MIDS  = False; class_weights={0:1, 1:15, 2:15, 3:15}
 
 tmp=MEGnet.__path__[0]
 dframe_path = op.join(tmp, 'prep_inputs','training', 'final_longform_dframe.csv')
@@ -111,8 +111,8 @@ else:
 # =============================================================================
 # Training parameters
 # =============================================================================
-NB_EPOCH = 400
-BATCH_SIZE = 700  
+NB_EPOCH = 200
+BATCH_SIZE = 500  
 VERBOSE = 1
 
 f1_score=F1Score(4, average='macro')
@@ -126,13 +126,29 @@ earlystop = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
                                  patience=4,
                                  restore_best_weights=True)
 
-if NORMALIZE==True:
-    print('Normalizing the data')
-    #MinMax timeseries
-    mm_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
-    arrTimeSeries = mm_scaler.fit_transform(arrTimeSeries.T).T * 15
-    #DO NOT Normalize Spatial -- Issues with this INT -> Float
 
+def prep_ts_blocks(arrTimeSeries, overlap=3750, normalize=True):
+    '''Cut the data into 1min blocks and temporally normalize each block
+    Currently assuming all datasets are the same number of samples'''
+    # Chunk Data
+    # num_blocks = 1 + (arrTimeSeries.shape[1] - 15000) // overlap
+    # starts = np.arange(num_blocks)*3750
+    starts = fGetStartTimesOverlap(arrTimeSeries.shape[1])
+    blocks = np.zeros([arrTimeSeries.shape[0], 15000, len(starts)], dtype=float)
+    for i,start in enumerate(starts): 
+        blocks[:,:, i] = copy.deepcopy(arrTimeSeries[:,start:start+15000]) 
+    
+    if normalize==True:
+        testTimeSeries = blocks #copy.deepcopy(arrScanTimeSeries[intStartTime:intStartTime+intModelLen])
+        min_vals = np.min(testTimeSeries, axis=1, keepdims=True)
+        max_vals = np.max(testTimeSeries, axis=1, keepdims=True)
+        scaling_factors = 10 / (max_vals - min_vals)
+        mean_vals = np.mean(testTimeSeries, axis=1, keepdims=True)
+        testTimeSeries = testTimeSeries - mean_vals
+        testTimeSeries = testTimeSeries * scaling_factors 
+    return testTimeSeries
+    
+    
 def get_train_test_idx(dframe, fold_number=0):
     '''
     dframe: input dataframe
@@ -145,9 +161,27 @@ def get_train_test_idx(dframe, fold_number=0):
     return {'train':train_idx, 'test':test_idx}
 
 
-cv = {i:get_train_test_idx(dframe,i) for i in range(8)}
+cv = {i:get_train_test_idx(dframe,i) for i in range(7)}
 history=[]
 
+# =============================================================================
+# Convert multistep sections of data to separate test
+# =============================================================================
+arrTS_blocks = prep_ts_blocks(arrTimeSeries, overlap=3750, normalize=True)
+num_steps = arrTS_blocks.shape[-1]
+arrTS_multi = arrTS_blocks.reshape([arrTS_blocks.shape[0]*arrTS_blocks.shape[-1], -1])
+arrSP_multi = np.repeat(arrSpatialMap, repeats=num_steps, axis=0)
+clID_multi = np.repeat(class_ID, repeats=num_steps, axis=0)
+
+dframe_list = []
+for i in range(num_steps):
+    tmp= copy.deepcopy(dframe)
+    tmp['ts_block']=i
+    dframe_list.append(tmp)
+dframe_multi = pd.concat(dframe_list)
+dframe_multi.reset_index(drop=True, inplace=True)
+
+assert arrTS_multi.shape[0] ==  arrSP_multi.shape[0] == clID_multi.shape[0] == len(dframe_multi)
 # =============================================================================
 # Burnin trainable layers - 1 round of initialization before possibly unfreeze
 # =============================================================================
@@ -166,15 +200,18 @@ if BURN_IN==True:
         optimizer=keras.optimizers.Adam(), 
         metrics=[f1_score, 'accuracy']
         )
-    tr = cv[0]['train'] #Train numpy indices axis0
-    te = cv[0]['test'] #Test numpy indices axis0
     
-    SP_tr, TS_tr, CL_tr   =  arrSpatialMap[tr,:,:,:], arrTimeSeries[tr,:], class_ID[tr]
-    SP_te, TS_te, CL_te   =  arrSpatialMap[te,:,:,:], arrTimeSeries[te,:], class_ID[te] 
+    tr = dframe_multi[dframe_multi['Fold0']=='Train'].index.values  
+    te = dframe_multi[dframe_multi['Fold0']=='Test'].index.values 
+    
+    SP_tr, TS_tr, CL_tr   =  arrSP_multi[tr,:,:,:], arrTS_multi[tr,:], clID_multi[tr]
+    SP_te, TS_te, CL_te   =  arrSP_multi[te,:,:,:], arrTS_multi[te,:], clID_multi[te]
+                   
     history_tmp = kModel.fit(x=dict(spatial_input=SP_tr, temporal_input=TS_tr), y=one_hot(CL_tr,4),
                          batch_size=BATCH_SIZE, epochs=NB_EPOCH, verbose=VERBOSE,  
                          validation_data=(dict(spatial_input=SP_te, temporal_input=TS_te), one_hot(CL_te,4)),
                          class_weight=class_weights, callbacks=[earlystop])
+
 
 # =============================================================================
 # Handle the FREEZE option
@@ -193,71 +230,76 @@ else:
 # =============================================================================
 hold_idx = dframe[dframe.HoldOut==True].index.values
 hold_sp, hold_ts, hold_clID = arrSpatialMap[hold_idx,:,:,:], arrTimeSeries[hold_idx,:], class_ID[hold_idx]
-arrTimeSeries=arrTimeSeries[:,:15000] #Temporary Hack
+# arrTimeSeries=arrTimeSeries[:,:15000] #Temporary Hack
+
 
 kModel.compile(
     loss=keras.losses.CategoricalCrossentropy(), 
-    optimizer=keras.optimizers.Adam(learning_rate=1e-5), 
+    optimizer=keras.optimizers.Adam(learning_rate=1e-3), 
     metrics=[f1_score, 'accuracy']
     )
 
 score_history=[]
 for cv_num in cv.keys():
-    tr = cv[cv_num]['train'] #Train numpy indices axis0
-    te = cv[cv_num]['test'] #Test numpy indices axis0
+    fold = f'Fold{cv_num}'
+    tr = dframe_multi[dframe_multi[fold]=='Train'].index.values  #cv[cv_num]['train'] #Train numpy indices axis0
+    te = dframe_multi[dframe_multi[fold]=='Test'].index.values #cv[cv_num]['test'] #Test numpy indices axis0
     
-    SP_tr, TS_tr, CL_tr   =  arrSpatialMap[tr,:,:,:], arrTimeSeries[tr,:], class_ID[tr]
-    SP_te, TS_te, CL_te   =  arrSpatialMap[te,:,:,:], arrTimeSeries[te,:], class_ID[te]  
+    SP_tr, TS_tr, CL_tr   =  arrSP_multi[tr,:,:,:], arrTS_multi[tr,:], clID_multi[tr]
+    SP_te, TS_te, CL_te   =  arrSP_multi[te,:,:,:], arrTS_multi[te,:], clID_multi[te]
                    
     history_tmp = kModel.fit(x=dict(spatial_input=SP_tr, temporal_input=TS_tr), y=one_hot(CL_tr,4),
                          batch_size=BATCH_SIZE, epochs=NB_EPOCH, verbose=VERBOSE,  
                          validation_data=(dict(spatial_input=SP_te, temporal_input=TS_te), one_hot(CL_te,4)),
                          class_weight=class_weights, callbacks=[earlystop])
+    save_weights_and_history(history_tmp, cv_num)
     history.append(history_tmp)
+    
     # score_history.append(fPredictChunkAndVoting(kModel, hold_ts, hold_sp, hold_clID))
 
 #%% Save outputs
-def save_weights_and_history(history):
-    for idx,epoch in enumerate(history):
-        epo_dir = op.join(output_dir, f'epoch{idx}')
-        os.mkdir(epo_dir)
-        with open(f'{epo_dir}/trainHistoryDict', 'wb') as file_pi:
-            pickle.dump(epoch.history, file_pi)
-        # with open(f'{epo_dir}/score', 'wb') as file_sc:
-        #     pickle.dump(score[idx], file_sc)
+def save_weights_and_history(history, kModel, cv_num):
+    # for idx,epoch in enumerate(history):
+    epo_dir = op.join(output_dir, f'epoch{str(cv_num)}')
+    os.mkdir(epo_dir)
+    with open(f'{epo_dir}/trainHistoryDict', 'wb') as file_pi:
+        pickle.dump(epoch.history, file_pi)
+    kModel.save(f'{epo_dir}/model')
+    # with open(f'{epo_dir}/score', 'wb') as file_sc:
+    #     pickle.dump(score[idx], file_sc)
 
-save_weights_and_history(history)
-kModel.save(f'{output_dir}/model')
+# save_weights_and_history(history)
+# kModel.save(f'{output_dir}/model')
     
 # =============================================================================
 #     Plot and save history
 # =============================================================================
 
-i=0;  j=0
-fig, axes = plt.subplots(3,3)
-for epo in range(8):
-    with open(f'{output_dir}/epoch{str(epo)}/trainHistoryDict', mode='rb') as w:
-        history = pickle.load(w)
-    if i==3:
-        i=0
-        j+=1
-    axes[j,i].plot(history['accuracy'], 'r')
-    axes[j,i].plot(history['f1_score'], 'g')    
-    axes[j,i].plot(history['val_accuracy'], 'b+')
-    axes[j,i].plot(history['val_f1_score'], 'k+')
-    axes[j,i].set_title(f'Epoch{str(epo)}')
-    i+=1
-fig.suptitle('TrAcc(red) TeAcc(blue) TrF1(green) TeF1(black)')
-fig.tight_layout()
-fig.savefig('TestTrain_graph.png', dpi=300) 
+# i=0;  j=0
+# fig, axes = plt.subplots(3,3)
+# for epo in range(8):
+#     with open(f'{output_dir}/epoch{str(epo)}/trainHistoryDict', mode='rb') as w:
+#         history = pickle.load(w)
+#     if i==3:
+#         i=0
+#         j+=1
+#     axes[j,i].plot(history['accuracy'], 'r')
+#     axes[j,i].plot(history['f1_score'], 'g')    
+#     axes[j,i].plot(history['val_accuracy'], 'b+')
+#     axes[j,i].plot(history['val_f1_score'], 'k+')
+#     axes[j,i].set_title(f'Epoch{str(epo)}')
+#     i+=1
+# fig.suptitle('TrAcc(red) TeAcc(blue) TrF1(green) TeF1(black)')
+# fig.tight_layout()
+# fig.savefig('TestTrain_graph.png', dpi=300) 
 
-score= fPredictChunkAndVoting(kModel, hold_ts, hold_sp, hold_clID)
-with open(f'score', 'wb') as file_sc:
-    pickle.dump(score, file_sc)
+# score= fPredictChunkAndVoting(kModel, hold_ts, hold_sp, hold_clID)
+# with open(f'score', 'wb') as file_sc:
+#     pickle.dump(score, file_sc)
 
-y_hat = kModel.predict(x=dict(spatial_input=hold_sp, temporal_input=hold_ts))
-y_pred = y_hat.argmax(axis=1)
-matrix = confusion_matrix(hold_clID, y_hat.argmax(axis=1))
-np.save(f'{output_dir}/confusion_mat.npy', matrix)
+# y_hat = kModel.predict(x=dict(spatial_input=hold_sp, temporal_input=hold_ts))
+# y_pred = y_hat.argmax(axis=1)
+# matrix = confusion_matrix(hold_clID, y_hat.argmax(axis=1))
+# np.save(f'{output_dir}/confusion_mat.npy', matrix)
 
 
