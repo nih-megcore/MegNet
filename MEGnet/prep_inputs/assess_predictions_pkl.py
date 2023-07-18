c5ff9f88654edaa6b75d3d8eb5ac34f42146fbfc
Jeff Stout <stoutjd@nih.gov>
2023-07-18 10:55:25 -0400

    Cleaned up the code and renamed

--- MEGnet/prep_inputs/assess_predictions_pkl.py
+++ MEGnet/prep_inputs/assess_predictions_pkl.py
@@ -17,6 +17,7 @@ import pandas as pd
 import pickle
 import MEGnet
 from MEGnet.megnet_utilities import fPredictChunkAndVoting, fGetStartTimesOverlap
+from MEGnet.megnet_utilities import fPredictChunkAndVoting_parrallel as fPredictChunkAndVoting
 import tensorflow_addons
 from sklearn.metrics import confusion_matrix, f1_score
 import sys
@@ -27,98 +28,13 @@ pkl_topdir = pkl_dir
 dframe_hold = pd.read_csv(op.join(pkl_topdir, 'dframe_holdout.csv'))
 
 # =============================================================================
-# assess predictions
-# =============================================================================
-
-def fPredictChunkAndVoting(kModel, lTimeSeries, arrSpatialMap, intModelLen=15000, intOverlap=3750):
-    """
-    This function is designed to take in ICA time series and a spatial map pair and produce a prediction useing a trained model.
-    The time series will be split into multiple chunks and the final prediction will be a weighted vote of each time chunk.
-    The weight for the voting will be determined by the manout of time and overlap each chunk has with one another.
-    For example if the total lenght of the scan is 50 seconds, and the chunks are 15 seconds long with a 5 second overlap:
-        The first chunk will be the only chunk to use the first 10 seconds, and one of two chunks to use the next 5 seconds.
-            Thus   
-
-    :param kModel: The model that will be used for the predictions on each chunk. It should have two inputs the spatial map and time series respectivley
-    :type kModel: a keras model
-    :param lTimeSeries: The time series for each scan (can also be an array if all scans are the same lenght)
-    :type lTimeSeries: list or array (if each scan is a different length, then it needs to be a list)
-    :param arrSpatialMap: The spatial maps (one per scan)
-    :type arrSpatialMap: numpy array
-    :param intModelLen: The lenght of the time series in the model, defaults to 15000
-    :type intModelLen: int, optional
-    :param intOverlap: The lenght of the overlap between scans, defaults to 3750
-    :type intOverlap: int, optional
-    """
-    #empty list to hold the prediction for each component pair
-    lPredictionsVote = []
-    lGTVote = []
-
-    lPredictionsChunk = []
-    lGTChunk = []
-    allChunkPredictions = []
-
-    i = 0
-    num_subjs = arrSpatialMap.shape[0]//20
-    arrSpatialMap = arrSpatialMap.reshape(arrSpatialMap.shape[0]//20, 20, 120,120,3)
-    lTimeSeries = lTimeSeries.reshape(lTimeSeries.shape[0]//20, 20, -1)
-    
-    for subj_idx in range(num_subjs):
-    # for arrScanTimeSeries, arrScanSpatialMap, arrScanY in zip(lTimeSeries, arrSpatialMap, arrY):
-        lPredictionsChunk=[]
-        arrScanTimeSeries=lTimeSeries[subj_idx, :,:]
-        arrScanSpatialMap = arrSpatialMap[subj_idx,:,:,:,:]
-        intTimeSeriesLen = lTimeSeries.shape[-1]
-        lStartTimes = fGetStartTimesOverlap(intTimeSeriesLen, intModelLen=intModelLen, intOverlap=intOverlap)
-
-        if lStartTimes[-1]+intModelLen <= intTimeSeriesLen:
-            lStartTimes.append(arrScanTimeSeries.shape[0]-intModelLen)
-            
-        lTimeChunks = [[x,x+intModelLen] for x in lStartTimes]
-        dctTimeChunkVotes = dict([[x,0] for x in lStartTimes])
-        for intT in range(intTimeSeriesLen):
-            lChunkMatches = [x <= intT < x+intModelLen for x in dctTimeChunkVotes.keys()]
-            intInChunks = np.sum(lChunkMatches)
-            for intStartTime, bTruth in zip(dctTimeChunkVotes.keys(), lChunkMatches):
-                if bTruth:
-                    dctTimeChunkVotes[intStartTime]+=1.0/intInChunks
-
-        #predict
-        dctWeightedPredictions = {}
-        dctTimeChunkVotes = {i:j for i,j in dctTimeChunkVotes.items() if i>0}
-        for intStartTime in dctTimeChunkVotes.keys():
-            testTimeSeries = copy.deepcopy(arrScanTimeSeries[:, intStartTime:intStartTime+intModelLen])
-            min_vals = np.min(testTimeSeries, axis=1, keepdims=True)
-            max_vals = np.max(testTimeSeries, axis=1, keepdims=True)
-            scaling_factors = 8 / (max_vals - min_vals)
-            mean_vals = np.mean(testTimeSeries, axis=1, keepdims=True)
-            testTimeSeries = testTimeSeries - mean_vals
-            testTimeSeries = testTimeSeries * scaling_factors 
-            
-            lPrediction = kModel.predict([arrScanSpatialMap,testTimeSeries])
-            lPredictionsChunk.append(lPrediction)
-            
-            dctWeightedPredictions[intStartTime] = lPrediction*dctTimeChunkVotes[intStartTime]
-
-        arrScanPrediction = np.stack(dctWeightedPredictions.values())
-        arrScanPrediction = arrScanPrediction.mean(axis=0)
-        arrScanPrediction = arrScanPrediction/arrScanPrediction.sum()
-        lPredictionsVote.append(arrScanPrediction)
-        allChunkPredictions.append(np.stack(lPredictionsChunk, axis=-1))
-        #print(f"{i}/{arrY.shape[0]}")
-        i+=1
-    lPredictionsVote = np.stack(lPredictionsVote)
-    lPredictionsVote = lPredictionsVote.reshape(lPredictionsVote.shape[0]*lPredictionsVote.shape[1],-1)
-    return lPredictionsVote , np.stack(allChunkPredictions) #, np.stack(lGTChunk)
-
-# =============================================================================
 # Run the prediction scoring
 # =============================================================================
-
 
 kModel_fname = op.join(MEGnet.__path__[0], 'model_v2')
 kModel = keras.models.load_model(kModel_fname, compile=False)
 
+## Load each pkl and numpy file for each dataset
 holdout = {}
 for idx, input_vec in dframe_hold.iterrows():
     outdir = op.join(pkl_dir, f'{input_vec.Site}_{input_vec.subjid}_{input_vec.TaskType}')
@@ -131,7 +47,7 @@ for idx, input_vec in dframe_hold.iterrows():
     with open(sp_file, 'rb') as g: holdout[idx]['sp'] = np.load(g)
     with open(cl_file, 'rb') as h: holdout[idx]['cl'] = np.load(h)
 
-
+## Assess the data using the keras model
 for idx in holdout.keys():  #These are the keys to the dictionary not dataframe
     arrTS = holdout[idx]['ts']
     arrSP = holdout[idx]['sp']
@@ -142,6 +58,7 @@ for idx in holdout.keys():  #These are the keys to the dictionary not dataframe
     holdout[idx]['arrProb'] = arrProb
 
 
+## Build out the stats from the assessments above
 ypred = np.hstack([holdout[i]['arrPred'] for i in holdout.keys()])
 cl = np.hstack([holdout[i]['cl'] for i in holdout.keys()])
 
