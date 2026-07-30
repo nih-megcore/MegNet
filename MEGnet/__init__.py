6905199956d84a15f95bc92577a05ebaef852803
Jeff Stout <stoutjd@nih.gov>
2026-07-30 10:40:03 -0400

    Make version number pull from install version [skip ci]

--- MEGnet/__init__.py
+++ MEGnet/__init__.py
@@ -1,10 +1,3 @@
 from . import prep_inputs
-import os.path as op
-
-version_path=op.join(op.dirname(__file__), 'version')
-
-
-with open(version_path) as f:
-    __version__=f.readlines()[0]
-    __version__=__version__.replace('\n','')
-#__version__='0.2'
+from importlib.metadata import version
+__version__ = version("MEGnet-neuro")
